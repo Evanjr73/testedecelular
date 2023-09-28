370a289b8272bb9febd325e4e5dbf41a0338a175
Evan junior <evsioljr@gmail.com>
2023-09-28 20:24:05 -0300

    Initial commit

---
+++ app/src/main/java/com/example/testedecelular/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.testedecelular
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var preco : EditText
+    private lateinit var btnCalcular : Button
+    private lateinit var resposta : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupView()
+        setupListeners()
+
+    }
+    fun setupView(){
+        preco = findViewById<EditText>(R.id.editTextText)
+
+        btnCalcular = findViewById<Button>(R.id.button)
+        resposta = findViewById<TextView>(R.id.textView)
+
+    }
+
+    fun setupListeners(){
+
+        btnCalcular.setOnClickListener {
+            var textodigitado = preco.text.toString().toFloat()
+
+            if(textodigitado.toInt() == 123){
+                //Log.d( "texto digitado", textodigitado.toString())
+                resposta.text= "sai dai sua louca "
+            }
+            if(textodigitado.toInt() != 123){
+                //Log.d( "texto digitado", textodigitado.toString())
+                resposta.text= "errou a senha "
+            }
+
+        }
+
+    }
+    
+}
+
+
